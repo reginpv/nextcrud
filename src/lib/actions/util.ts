a7daf8b198998536e6e356f056dff80e8be7901a
reginpv <reginpv@gmail.com>
2025-10-27 07:40:19 +0800

    feat: account recovery forgot and reset pass

--- src/lib/actions/util.ts
+++ src/lib/actions/util.ts
@@ -14,6 +14,7 @@ import { isValidEmail } from '../helper'
 
 const table = 'resetPasswordToken'
 
+// FORGOT PASSWORD
 export async function forgotPassword(_prevState: any, formData: FormData) {
   const email = formData.get('email')?.toString().trim()
 
@@ -119,6 +120,129 @@ export async function forgotPassword(_prevState: any, formData: FormData) {
       success: false,
       payload: null,
       message: 'Failed to send email',
+    }
+  }
+}
+
+// RESET PASSWORD
+export async function resetPassword(_prevState: any, formData: FormData) {
+  const token = formData.get('token')?.toString().trim()
+  const email = formData.get('email')?.toString().trim()
+  const password = formData.get('password')?.toString().trim()
+  const confirmPassword = formData.get('confirmPassword')?.toString().trim()
+
+  console.log('token: ', token)
+  console.log('email: ', email)
+  console.log('password: ', password)
+  console.log('confirmPassword: ', confirmPassword)
+
+  // Validate
+  if (!token || !email || !password) {
+    return {
+      success: false,
+      payload: null,
+      message: 'All fields are required.',
+    }
+  }
+
+  // Confirm
+  if (password !== confirmPassword) {
+    return {
+      success: false,
+      payload: null,
+      message: 'Passwords do not match.',
+    }
+  }
+
+  try {
+    // Find the token in the resetpasswordtoken table
+    const resetToken = await prisma[table].findUnique({
+      where: { token },
+    })
+
+    console.log('reset token: ', resetToken)
+
+    if (!resetToken || resetToken.email !== email) {
+      return {
+        success: false,
+        payload: null,
+        message: 'Invalid or expired token.',
+      }
+    }
+
+    if (resetToken.expires < new Date()) {
+      return {
+        success: false,
+        payload: null,
+        message: 'Token has expired.',
+      }
+    }
+
+    // Hash the new password
+    const hashedPassword = await hash(password, 12)
+
+    // Update the user's password in the database
+    await prisma.user.update({
+      where: { email },
+      data: { password: hashedPassword },
+    })
+
+    // Optionally, remove the reset token from the database after successful reset
+    await prisma[table].delete({
+      where: { token },
+    })
+
+    // SMTP: Send an email to notify user that their password has been reset
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: 465,
+      secure: true, //process.env.SMTP_SECURE,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_KEY,
+      },
+    })
+
+    const content = `
+			<p>Hi ${email},</p>
+			<p>Your password has been successfully reset.</p>
+			<p>If you did not perform this action, please contact our support team immediately.</p>
+			<p>Thank you!</p>
+		`
+
+    const mailOptions = {
+      from: `${SMTP_FROM_NAME} <${SMTP_FROM_EMAIL}>`,
+      to: email,
+      subject: `Password Reset Successful - ${APP_NAME}`,
+      html: defaultEmailTemplate(content),
+    }
+
+    const mail = await transporter.sendMail(mailOptions)
+
+    console.log('mail: ', mail)
+
+    if (!mail.accepted.length) {
+      return {
+        sucess: false,
+        payload: null,
+        message: 'Failed to send email.',
+      }
+    } else {
+      return {
+        success: true,
+        payload: null,
+        message: 'Password reset successful.',
+      }
+    }
+
+    //
+  } catch (error) {
+    console.error('Error in resetPassword function: ', error)
+
+    return {
+      success: false,
+      payload: null,
+      message: 'Failed to reset password',
     }
   }
 }
