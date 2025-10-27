372d541d48aecc181af8c70128d964dba4d829fd
reginpv <reginpv@gmail.com>
2025-10-27 14:01:26 +0800

    Update homepage content

--- src/templates/Default.tsx
+++ src/templates/Default.tsx
@@ -1,17 +1,16 @@
-import ThemeProvider from "./themeProvider"
-import Header from "@/components/globals/Header"
-import Footer from "@/components/globals/Footer"
+import ThemeProvider from './themeProvider'
+import Header from '@/components/globals/Header'
+import Footer from '@/components/globals/Footer'
 
-export default async function TemplateDefault({ 
-  children 
-}:{
+export default async function TemplateDefault({
+  children,
+}: {
   children: React.ReactNode
 }) {
-
   return (
     <ThemeProvider>
       <Header />
-      <main className="dark:bg-gray-800 dark:text-white">{children}</main>
+      <main className="bg-primary">{children}</main>
       <Footer />
     </ThemeProvider>
   )
